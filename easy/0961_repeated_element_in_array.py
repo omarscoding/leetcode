c0e50c29b0d1214be882e5e998f43598798c7886
omarscoding <omaryoussefnz@gmail.com>
2026-01-02 20:26:28 +1300

    leetcode 0961

---
+++ easy/0961_repeated_element_in_array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def repeatedNTimes(self, nums):
+        seen = set()
+        for i in nums:
+            if i in seen:
+                return i
+            seen.add(i)
+
