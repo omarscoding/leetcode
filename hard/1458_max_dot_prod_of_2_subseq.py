a0f3be5c80057e14f0e61dc4d1e9f3c791fa48ee
omarscoding <omaryoussefnz@gmail.com>
2026-01-09 03:00:31 +1300

    1458, daily leetcode jan 8

---
+++ hard/1458_max_dot_prod_of_2_subseq.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxDotProduct(self, nums1, nums2):
+        if max(nums1) < 0 and min(nums2) > 0:
+            return max(nums1) * min(nums2)
+
+        if max(nums2) < 0 and min(nums1) > 0:
+            return max(nums2) * min(nums1)
+
+        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+        for i in range(len(nums1) - 1, -1, -1):
+            for j in range(len(nums2) - 1, -1, -1):
+                use = nums1[i] * nums2[j] + dp[i + 1][j + 1]
+                dp[i][j] = max (use, dp[i + 1][j], dp[i][j + 1])
+
+        return dp[0][0]
+    
