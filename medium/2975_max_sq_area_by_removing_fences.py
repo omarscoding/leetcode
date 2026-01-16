69efdb6140cf78441dcdcdb9e0ca7b1448e678df
omarscoding <omaryoussefnz@gmail.com>
2026-01-17 02:10:36 +1300

    2975, daily leetcode jan 16

---
+++ medium/2975_max_sq_area_by_removing_fences.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maximizeSquareArea(self, m, n, hFences, vFences):
+        MOD = 10**9 + 7
+
+        h = sorted(hFences + [1, m])
+        v = sorted(vFences + [1, n])
+
+        h_dist = set()
+        for i in range(len(h)):
+            for j in range(i + 1, len(h)):
+                h_dist.add(h[j] - h[i])
+
+        best = -1
+        for i in range(len(v)):
+            for j in range(i + 1, len(v)):
+                d = v[j] - v[i]
+                if d in h_dist:
+                    if d > best:
+                        best = d
+
+        if best == -1:
+            return -1
+
+        return (best * best) % MOD
