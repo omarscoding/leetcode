0445c89aab978d02011276aa9e196e28e076543a
omarscoding <omaryoussefnz@gmail.com>
2026-01-26 23:34:57 +0800

    1200, daily leetcode jan 26

---
+++ easy/1200_min_abs_diff.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minimumAbsDifference(self, arr):
+        arr.sort()
+        n = len(arr)
+        minDiff = arr[n-1] - arr[n-2]
+        for i in range(1, n):
+            minDiff = min(minDiff, arr[i] - arr[i-1])
+        ans = []
+        for i in range(1, n):
+            if arr[i] - arr[i-1] == minDiff:
+                ans.append([arr[i-1], arr[i]])
+        return ans
+        
