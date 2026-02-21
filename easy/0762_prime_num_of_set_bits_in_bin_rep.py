dff634e6a2935959c274f2b6d11dd53eac854d6c
omarscoding <omaryoussefnz@gmail.com>
2026-02-22 02:34:43 +1300

    762, daily leetcode feb 21

---
+++ easy/0762_prime_num_of_set_bits_in_bin_rep.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        res = 0
+        for num in range(left, right + 1):
+            bin_num = bin(num)
+            one_count = bin_num.count("1")
+
+            if self.is_prime(one_count):
+                res += 1
+        
+        return res
+       
+    def is_prime(self, n):
+            if n < 2:
+                return False
+            for i in range(2, int(math.sqrt(n)) + 1):
+                if n % i == 0:
+                    return False
+            return True
+            
+    
